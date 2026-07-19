a9ae7967a3e2b4402d245a81edbc08536ab268dd
Aman Saikia <100982873+MnSykia@users.noreply.github.com>
2026-07-19 13:55:26 +0530

    Update next.config.js to remove ESLint settings
    Removed ESLint configuration and adjusted TypeScript settings.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // Warning: Dangerously allow production builds to successfully complete
+    // Dangerously allow production builds to successfully complete
     // even if your project has type errors.
     ignoreBuildErrors: true,
   },
