4a1940ab70e22459b15dc321e256c289d39f75b6
Aman Saikia <100982873+MnSykia@users.noreply.github.com>
2026-07-19 13:50:58 +0530

    Configure ESLint and TypeScript error handling
    Added ESLint and TypeScript configuration options to ignore errors during production builds.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: Dangerously allow production builds to successfully complete
+    // even if your project has type errors.
+    ignoreBuildErrors: true,
   },
 };
 
